efe9ee083233be5f53bfed90ec11256c39549db6
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2025-04-24 16:11:40 +0300

    Урок 88

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,44 +3,44 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
-let employersNames = [];
+// let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+// let employersNames = [];
 
-function convertData() {
-	for (let i = 0; i < employers.length; i++) {
-		let elem = employers[i];
-		if (elem.length > 0 && elem.length != '') {
-			employersNames.push(elem.toLowerCase().trim());
-		}
-	}
-	employersNames = employersNames.map(item => `${item[0].toUpperCase()}${item.slice(1)}`);
-}
-convertData();
+// function convertData() {
+// 	for (let i = 0; i < employers.length; i++) {
+// 		let elem = employers[i];
+// 		if (elem.length > 0 && elem.length != '') {
+// 			employersNames.push(elem.toLowerCase().trim());
+// 		}
+// 	}
+// 	employersNames = employersNames.map(item => `${item[0].toUpperCase()}${item.slice(1)}`);
+// }
+// convertData();
 
-const sponsors = {
-	cash: [40000, 5000, 30400, 12000],
-	eu: ['SRL', 'PLO', 'J&K'],
-	rus: ['RusAuto', 'SBO']
-};
+// const sponsors = {
+// 	cash: [40000, 5000, 30400, 12000],
+// 	eu: ['SRL', 'PLO', 'J&K'],
+// 	rus: ['RusAuto', 'SBO']
+// };
 
-function calcCash(everyCash, own = 0) {
-	let total = 0;
-	everyCash.forEach(el => {
-		total += el;
-	});
-	return total;
-}
+// function calcCash(everyCash, own = 0) {
+// 	let total = 0;
+// 	everyCash.forEach(el => {
+// 		total += el;
+// 	});
+// 	return total;
+// }
 
-const money = calcCash(sponsors.cash);
+// const money = calcCash(sponsors.cash);
 
-function makeBusiness(owner, cash, emp, director = 'Victor') {
-	const sumSponsors = [...sponsors.eu, ...sponsors.rus, 'unexpected sponsor'];
-	console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp.join(', ')}`);
-	console.log('And we have a sponsors: ');
-	console.log(`${sumSponsors.join(' ')}`);
-	console.log(`Note. Be careful with ${sponsors.eu[0]}. It's a huge risk.`);
-}
-makeBusiness('Sam', money, employersNames);
+// function makeBusiness(owner, cash, emp, director = 'Victor') {
+// 	const sumSponsors = [...sponsors.eu, ...sponsors.rus, 'unexpected sponsor'];
+// 	console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp.join(', ')}`);
+// 	console.log('And we have a sponsors: ');
+// 	console.log(`${sumSponsors.join(' ')}`);
+// 	console.log(`Note. Be careful with ${sponsors.eu[0]}. It's a huge risk.`);
+// }
+// makeBusiness('Sam', money, employersNames);
 
 // var employersNames = [];
 // for (var i = 0; i < employers.length; i++) {
--- src/js/react/Index.jsx
+++ src/js/react/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"
+// import App from "./App.jsx"
 
 
 // Объект для вывода
@@ -8,9 +8,18 @@ const root = document.querySelector("#root")
 	? document.querySelector("#root")
 	: document.querySelector(".wrapper");
 
+const elem = (
+	<div>
+		<h2>Hellow World</h2>
+		<input type="text" placeholder="Type here" />
+		<button />
+	</div>
+)
+
+
 // Main rendering
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
-		<App />
+		{elem}
 	</React.StrictMode>
 );
